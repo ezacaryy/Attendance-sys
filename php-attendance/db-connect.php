da164af1756ae6705bb23ad3fa84708a0e44b45f
Zakaria Cheikh <133505628+ezacaryy@users.noreply.github.com>
2024-06-15 01:24:24 +0100

    Add files via upload

---
+++ php-attendance/db-connect.php
@@ -0,0 +1,10 @@
+<?php $host = "localhost"; $uname = "root"; $pw = ""; $dbname = "simple_attendance_db";
+
+try{
+    $conn = new MySQLi($host, $uname, $pw, $dbname);
+}catch(Exception $e){
+    echo "Database Connection Failed: <br>";
+    print_r($e->getMessage());
+    exit;
+}
+?>
